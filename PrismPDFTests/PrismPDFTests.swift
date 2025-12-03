24227653a118f8bc7a8f14f1558384e051457c80
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2025-12-03 12:33:56 +0700

    Initial Commit

---
+++ PrismPDFTests/PrismPDFTests.swift
@@ -0,0 +1,17 @@
+//
+//  PrismPDFTests.swift
+//  PrismPDFTests
+//
+//  Created by Azzam Ubaidillah on 03/12/25.
+//
+
+import Testing
+@testable import PrismPDF
+
+struct PrismPDFTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
